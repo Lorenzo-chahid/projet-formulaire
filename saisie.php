e4abe5010cd0018d1ccbe66f318224d4b9a5aa29
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-12-04 16:09:11 +0100

    try for mailer 2

--- saisie.php
+++ saisie.php
@@ -7,36 +7,32 @@
     <title>Document</title>
 
     <?php
-$curl = curl_init();
-$name = $_POST['name']; 
-$email = $_POST['email']; 
-$subject = $_POST['subject']; 
-$message = $_POST['message'];
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n  \"personalizations\": [\n    {\n      \"to\": [\n        {\n          \"email\": \"[Email Address to send Contact to]\"\n        }\n      ],\n      \"subject\": \"New Contact\"\n    }\n  ],\n  \"from\": {\n    \"email\": \"[FROM EMAIL]\"\n  },\n  \"content\": [\n    {\n      \"type\": \"text/html\",\n      \"value\": \"$name<br>$email<br>$subject<br>$message\"\n    }\n  ]\n}",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer [SG API key]",
-    "cache-control: no-cache",
-    "content-type: application/json"
-  ),
-));
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-header('Location: thanks.html');
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+//require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+require("sendgrid-php/sendgrid-php.php");
+// If not using Composer, uncomment the above line and
+// download sendgrid-php.zip from the latest release here,
+// replacing <PATH TO> with the path to the sendgrid-php.php file,
+// which is included in the download:
+// https://github.com/sendgrid/sendgrid-php/releases
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
 }
-?>
+
 
 
 

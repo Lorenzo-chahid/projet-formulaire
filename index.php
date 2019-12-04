20550c5faead7780243dacc16dd510c0dc06847f
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-12-04 18:24:51 +0100

    trash try

--- index.php
+++ index.php
@@ -191,9 +191,11 @@ function autoplay() {
 
     <!--PHP DU FORMULAIRE -->
 <?php
-    //require 'vendor/autoload.php'; // If you're using Composer (recommended)
+$SENDGRID_API_KEY='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';
+
+    require 'vendor/autoload.php'; // If you're using Composer (recommended)
     // Comment out the above line if not using Composer
-    require("sendgrid-php/sendgrid-php.php");
+    //require("sendgrid-php/sendgrid-php.php");
     // If not using Composer, uncomment the above line and
     // download sendgrid-php.zip from the latest release here,
     // replacing <PATH TO> with the path to the sendgrid-php.php file,

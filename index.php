5cfc67da5a240eefe968a08c13b4bcd3dc9502fa
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-12-04 18:53:04 +0100

    try again again with env

--- index.php
+++ index.php
@@ -193,10 +193,10 @@ function autoplay() {
 <?php
 
 
-    //require 'vendor/autoload.php'; // If you're using Composer (recommended)
+    require 'vendor/autoload.php'; // If you're using Composer (recommended)
     $SENDGRID_API_KEY='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';
     // Comment out the above line if not using Composer
-    require("sendgrid-php/sendgrid-php.php");
+    //require("sendgrid-php/sendgrid-php.php");
     // If not using Composer, uncomment the above line and
     // download sendgrid-php.zip from the latest release here,
     // replacing <PATH TO> with the path to the sendgrid-php.php file,

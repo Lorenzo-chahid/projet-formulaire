7157fc6c6175f24cfd8815b6ca51ead18fba428e
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-12-05 21:45:21 +0100

    script changement

--- index.php
+++ index.php
@@ -297,9 +297,10 @@
     
 
    
-   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+   
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
    <script src="assets/js/script.js"></script>
+   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
    
 </body>
 </html>

780f36b880e653f48d47535d309e68f7c9ca5e28
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-12-07 16:24:59 +0100

    place php

--- index.php
+++ index.php
@@ -181,105 +181,6 @@ document.addEventListener('DOMContentLoaded', function() {
 </section>
 
 
-    <!--PHP DU FORMULAIRE -->
-<?php
-   
-
-
-    require 'vendor/autoload.php'; // If you're using Composer (recommended)
-    
-    // Comment out the above line if not using Composer
-    //require("sendgrid-php/sendgrid-php.php");
-    // If not using Composer, uncomment the above line and
-    // download sendgrid-php.zip from the latest release here,
-    // replacing <PATH TO> with the path to the sendgrid-php.php file,
-    // which is included in the download:
-    // https://github.com/sendgrid/sendgrid-php/releases
-    $options = array(
-      'firstName' 	=> FILTER_SANITIZE_STRING,
-      'secondName' 	=> FILTER_SANITIZE_STRING,
-      'idMail' 		    => FILTER_VALIDATE_EMAIL,
-      'subject' 		=> FILTER_SANITIZE_STRING,
-      'country' 		=> FILTER_SANITIZE_STRING);
-     
-
-      $result = filter_input_array(INPUT_POST, $options);  
-
-      if ($result != null AND $result != FALSE) {
-
-        echo "Tous les champs ont été nettoyés !";
-      
-      } else {
-      
-        echo "Un champ est vide ou n'est pas correct!";
-      
-      }
-      foreach($options as $key => $value) 
-      {
-         $result[$key]=trim($result[$key]);
-         
-      }
-  
-      if(isset($_POST['fake-field']) && $_POST['fake-field'] != ''){
-                header('Location: http://www.monsite.com/fake-confirmation/');
-                exit('Redirecting you to http://www.monsite.com/fake-confirmation/');
-      } else {
-        if(isset($_POST["sendemail"])){
-          $name = $_POST["firstName"];
-          $secondName = $_POST["secondName"];
-          $city = $_POST["ville"];
-          $mail = $_POST["idEmail"];
-    
-          $email = new \SendGrid\Mail\Mail(); 
-          $email->setFrom("chahid.lorenzo@outlook.com", "Lorenzo");
-          $email->setSubject("Sending with SendGrid is Fun");
-          $email->addTo($mail, $name);
-          $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-          $email->addContent(
-              "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-          );
-          $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
-          //if($sendgrid->send($email)){
-           // echo " Email sent SuccessFully";
-          //}else{
-          //  echo "errors";
-          //}
-        
-         try {
-             $response = $sendgrid->send($email);
-            //print $response->statusCode() . "\n";
-            //print_r($response->headers());
-            //print $response->body() . "\n";
-            echo "SEND EMAIL WITH SUCCES";
-         } catch (Exception $e) {
-            echo 'Caught exception: '. $e->getMessage() ."\n";
-         }
-        }
-    
-        try{
-          $bdd = new PDO('mysql:host=localhost;dbname=form_php;charset=utf8', 'root', 'root');
-        } catch(Exception $e) {
-            die('Erreur : '.$e->getMessage());
-        }
-        
-        $req = $bdd->prepare('INSERT INTO hackers_poulette(nom, prenom, ville, email) VALUES(:nom, :prenom, :ville, :email)');
-        $req->execute(array(
-            'nom' => $name = $_POST["firstName"],
-            'prenom' => $secondName = $_POST["secondName"],
-            'ville' => $city = $_POST["ville"],
-            'email' => $mail = $_POST["idEmail"],
-            
-            ));
-        
-        echo 'Les données ont bien été ajouté !';
-      
-      }
-
-    
-
-
-
-?>
   
 
 <!-- SEND EMAIL BY SENDGRID -->
@@ -356,6 +257,106 @@ document.addEventListener('DOMContentLoaded', function() {
         
     
 </section>
+  <!--PHP DU FORMULAIRE -->
+  <?php
+   
+
+
+   require 'vendor/autoload.php'; // If you're using Composer (recommended)
+   
+   // Comment out the above line if not using Composer
+   //require("sendgrid-php/sendgrid-php.php");
+   // If not using Composer, uncomment the above line and
+   // download sendgrid-php.zip from the latest release here,
+   // replacing <PATH TO> with the path to the sendgrid-php.php file,
+   // which is included in the download:
+   // https://github.com/sendgrid/sendgrid-php/releases
+   $options = array(
+     'firstName' 	=> FILTER_SANITIZE_STRING,
+     'secondName' 	=> FILTER_SANITIZE_STRING,
+     'idMail' 		    => FILTER_VALIDATE_EMAIL,
+     'subject' 		=> FILTER_SANITIZE_STRING,
+     'country' 		=> FILTER_SANITIZE_STRING);
+    
+
+     $result = filter_input_array(INPUT_POST, $options);  
+
+     if ($result != null AND $result != FALSE) {
+
+       echo "Tous les champs ont été nettoyés !";
+     
+     } else {
+     
+       echo "Un champ est vide ou n'est pas correct!";
+     
+     }
+     foreach($options as $key => $value) 
+     {
+        $result[$key]=trim($result[$key]);
+        
+     }
+ 
+     if(isset($_POST['fake-field']) && $_POST['fake-field'] != ''){
+               header('Location: http://www.monsite.com/fake-confirmation/');
+               exit('Redirecting you to http://www.monsite.com/fake-confirmation/');
+     } else {
+       if(isset($_POST["sendemail"])){
+         $name = $_POST["firstName"];
+         $secondName = $_POST["secondName"];
+         $city = $_POST["ville"];
+         $mail = $_POST["idEmail"];
+   
+         $email = new \SendGrid\Mail\Mail(); 
+         $email->setFrom("chahid.lorenzo@outlook.com", "Lorenzo");
+         $email->setSubject("Sending with SendGrid is Fun");
+         $email->addTo($mail, $name);
+         $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+         $email->addContent(
+             "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+         );
+         $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
+         //if($sendgrid->send($email)){
+          // echo " Email sent SuccessFully";
+         //}else{
+         //  echo "errors";
+         //}
+       
+        try {
+            $response = $sendgrid->send($email);
+           //print $response->statusCode() . "\n";
+           //print_r($response->headers());
+           //print $response->body() . "\n";
+           echo "SEND EMAIL WITH SUCCES";
+        } catch (Exception $e) {
+           echo 'Caught exception: '. $e->getMessage() ."\n";
+        }
+       }
+   
+       try{
+         $bdd = new PDO('mysql:host=localhost;dbname=form_php;charset=utf8', 'root', 'root');
+       } catch(Exception $e) {
+           die('Erreur : '.$e->getMessage());
+       }
+       
+       $req = $bdd->prepare('INSERT INTO hackers_poulette(nom, prenom, ville, email) VALUES(:nom, :prenom, :ville, :email)');
+       $req->execute(array(
+           'nom' => $name = $_POST["firstName"],
+           'prenom' => $secondName = $_POST["secondName"],
+           'ville' => $city = $_POST["ville"],
+           'email' => $mail = $_POST["idEmail"],
+           
+           ));
+       
+       echo 'Les données ont bien été ajouté !';
+     
+     }
+
+   
+
+
+
+?>
+ 
 
     
 
